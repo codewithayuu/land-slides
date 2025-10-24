8ee98eac0ed1acdb3568e522a9fb30c2fb3764ed
Ayush Jha <codewithayuu@gmail.com>
2025-10-25 03:33:52 +0530

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,9 @@
-import MapView from "./components/MapView";
+import dynamic from "next/dynamic";
+
+const MapView = dynamic(() => import("./components/MapView"), {
+  ssr: false,
+  loading: () => <div style={{ padding: 16 }}>Loading map…</div>,
+});
 
 export default function Page() {
   return (
